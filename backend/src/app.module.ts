53e9cbe9f24e62ba5ff7e24e21bfd280293e277b
Safaa <Safaeettalhi@gmail.com>
2026-01-22 10:23:14 +0100

    ajout de l'importation du module Documents et de la configuration de l'environnement dans AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,13 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { DocumentsModule } from './documents/documents.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import envConfig from './config/env.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [envConfig],
       envFilePath: '.env',
     }),
     MongooseModule.forRoot(
@@ -17,6 +20,7 @@ import { AppService } from './app.service';
     ),
     AuthModule,
     UsersModule,
+    DocumentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
